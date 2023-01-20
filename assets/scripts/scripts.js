57550730a53bbc9b357a541c9068cfdba80974bf
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-01-20 19:34:34 +0200

    Update to script.js background colour for current time classes

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,2 +1,39 @@
+// Day, date and month in header
 var dayYear = moment().format('dddd D MMMM');
 $("#currentDay").text(dayYear);
+
+$(document).ready(function () {
+
+});
+
+// Get current time hour value
+var timeCurrent = moment().hour();
+
+// Loop timeCurrent over each hourly block
+$('.time-block').each(function() {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+    // then compare current time to each block to add .past, .present and .future classes for the background colour
+    if (timeCurrent === blockTime) {
+        $(this).addClass("present");
+    } else if (timeCurrent < blockTime) {
+        $(this).addClass("future");
+    } else {
+        $(this).addClass("past");
+    }
+  });
+
+
+// Save button event listener
+
+$(".saveBtn").on("click", function() {
+
+})
+
+// Get nearby values of the task/descriptor
+
+// Save the task/descriptor in local storage
+localStorage.setItem(time, text);
+
+// Get item from local storage if any
+
+
