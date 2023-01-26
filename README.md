a33a26fc22ed24710f4fa29e6623469d993ea76b
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-01-26 08:38:38 +0200

    Updates to README and amendment to scripts.js file

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# work-day-scheduler
+# Weekly Challenge 7
+
+## Daily Task & Appointment Scheduler 
+
+## Contents
+* [Description](#description)
+* [Approach](#approach)
+* [Challenges](#challenges)
+* [Features](#features)
+
+
+## Description
+A daily scheduler of tasks and appointments saved in hourly blocks locally to your browser.
+
+## Links
+You can find the application for reference here [Daily Task and Appointment Scheduler](https://urbanpatrol.github.io/work-day-scheduler/index.html)
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+The app should:
+
+* Display the current day at the top of the calender when a user opens the planner.
+* Present timeblocks for standard business hours when the user scrolls down.
+* Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+* Allow a user to enter an event when they click a timeblock
+* Save the event in local storage when the save button is clicked in that timeblock.
+* Persist events between refreshes of a page
+
+The following animation demonstrates the application functionality:
+
+![A user clicks on slots on the color-coded calendar and edits the events.](./images/05-third-party-apis-homework-demo.gif)
+
+## Approach
+TBC
+
+
+## Challenges
+
+
+## Credits
+* StackOverflow
+* GeekforGeeks
+
+## License
+n/a
+
+## Features
+TBC
+
+
+## Tests
+UAT Self
+
+---
+
+© 2022 Ramon Fritz. Confidential and Proprietary. All Rights Reserved.
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,39 +2,57 @@
 var dayYear = moment().format('dddd D MMMM');
 $("#currentDay").text(dayYear);
 
-$(document).ready(function () {
-
-});
-
+function trackTime() {
 // Get current time hour value
 var timeCurrent = moment().hour();
 
-// Loop timeCurrent over each hourly block
-$('.time-block').each(function() {
+// Loop time over each hourly block
+$(".time-block").each(function() {
     var blockTime = parseInt($(this).attr("class").split("hour")[1]);
     // then compare current time to each block to add .past, .present and .future classes for the background colour
-    if (timeCurrent === blockTime) {
-        $(this).addClass("present");
-    } else if (timeCurrent < blockTime) {
-        $(this).addClass("future");
-    } else {
+    if (blockTime < timeCurrent) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
         $(this).addClass("past");
     }
-  });
+    else if (blockTime === timeCurrent) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+  })
+}
 
-
+$(document).ready(function () {
 // Save button event listener
 $(".saveBtn").on("click", function() {
 
-})
-
 // Get nearby values of the task/descriptor
+// looks at the parent id and siblings(new) associated with the class "description"
 var time = $(this).parent().attr("id");
-var text = $(this).children("description").val();
+var text = $(this).siblings(".description").val();
 
 // Save the task/descriptor in local storage
 localStorage.setItem(time, text);
+})
+
 
 // Get item/s from local storage if any
+$("#09-block .description").val(localStorage.getItem("09-block"));
+$("#10-block .description").val(localStorage.getItem("10-block"));
+$("#11-block .description").val(localStorage.getItem("11-block"));
+$("#12-block .description").val(localStorage.getItem("12-block"));
+$("#13-block .description").val(localStorage.getItem("13-block"));
+$("#14-block .description").val(localStorage.getItem("14-block"));
+$("#15-block .description").val(localStorage.getItem("15-block"));
+$("#16-block .description").val(localStorage.getItem("16-block"));
+$("#17-block .description").val(localStorage.getItem("17-block"));
+//console.log("09-block");
 
-
+trackTime();
+})
